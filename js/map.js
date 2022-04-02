c6675ed40700fbbdfcfa5dac14044057d991fb61
CAPO\linn.tunberg <linn.tunberg@capo.se>
2022-04-02 19:44:54 +0200

    Initial commit

---
+++ js/map.js
@@ -0,0 +1,28 @@
+// // Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const weddingCoordinates = { lat: 58.245012, lng: 11.459485 };
+//   // The map, centered at Uluru
+//   const wedding = new google.maps.Map(document.getElementById("wedding"), {
+//     zoom: 4,
+//     center: weddingCoordinates,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: weddingCoordinates,
+//     map: wedding,
+//   });
+
+//   // The location of Uluru
+//   const partyCoordinates = { lat: 58.245012, lng: 11.459485 };
+//   // The map, centered at Uluru
+//   const party = new google.maps.Map(document.getElementById("party"), {
+//     zoom: 4,
+//     center: partyCoordinates,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker2 = new google.maps.Marker({
+//     position: partyCoordinates,
+//     map: party,
+//   });
+// }
